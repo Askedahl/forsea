92b7aaecdbe2ab4595fc08ae3ff4208bd500cb2b
Askedahl <32635310+Askedahl@users.noreply.github.com>
2019-12-09 09:24:57 +0100

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,51 @@
+var myWave = $('#myID').wavify({
+  height: 22,
+  bones: 3,
+  amplitude: 60,
+  color: '#73BF9C',
+  speed: .25
+});
+var myWave = $('#myID1').wavify({
+  height: 50,
+  bones: 4,
+  amplitude: 80,
+  color: '#027373',
+  speed: .17
+});
+var myWave = $('#myID2').wavify({
+  height: 75,
+  bones: 3,
+  amplitude: 100,
+  color: '#F5F5F7',
+  speed: .10
+});
+
+var myWave = $('#myID3').wavify({
+    height: 60,
+    bones: 2,
+    amplitude: 100,
+    color: '#73BF9C',
+    speed: .15
+  });
+  var myWave = $('#myID5').wavify({
+    height: 75,
+    bones: 3,
+    amplitude: 100,
+    color: '#F5F5F7',
+    speed: .10
+  });
+
+var myWave = $('#myID6').wavify({
+    height: 60,
+    bones: 2,
+    amplitude: 100,
+    color: '#73BF9C',
+    speed: .15
+  });
+  var myWave = $('#myID8').wavify({
+    height: 75,
+    bones: 3,
+    amplitude: 100,
+    color: '#F5F5F7',
+    speed: .10
+  });
